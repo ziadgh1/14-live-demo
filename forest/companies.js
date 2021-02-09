a2f427c55789447b6fbd902333d57a8f2a9ee980
ziadgh1 <ziadg@forestadmin.com>
2021-02-09 23:49:10 +0100

    fixed smart field in companies

--- forest/companies.js
+++ forest/companies.js
@@ -41,19 +41,6 @@ collection('companies', {
     type: 'bulk',
   },
   ],
-  fields: [{
-    // field: 'Description video',
-    // type: 'String',
-    // get: () => '<video controls src="http://localhost:3000/forest/actions/play-video" width="540"></video>',
-    // get: () => `<iframe width="540" height="315" src="https://www.youtube.com/embed/vdKbdMVsHN0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
-    // get: () => '<iframe width="540" height="315" src="https://stream.mux.com/pZUcG8r5kbDUNSpFoKgvfgaFAe01ZUE9FYyxSb9piCy4.m3u8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-    // get: () => '<script>$.getScript("https://cdn.jsdelivr.net/npm/video.js@7.10.2/dist/video.cjs.min.js", () => { this.set("loaded", true); }</script> <video controls src="https://storage.googleapis.com/muxdemofiles/mux-video-intro.mp4" width="540"></video>',
-    // get: () => '<video controls width="540" src="https://stream.mux.com/pZUcG8r5kbDUNSpFoKgvfgaFAe01ZUE9FYyxSb9piCy4.m3u8" type="application/x-mpegURL"></video>',
-    // get: () => '<script>$.getScript("https://cdn.jsdelivr.net/npm/video.js@7.10.2/dist/video.cjs.min.js", () => { this.set("loaded", true); }</script><script><video controls width="540" data-setup="{}"><source src="https://stream.mux.com/pZUcG8r5kbDUNSpFoKgvfgaFAe01ZUE9FYyxSb9piCy4.m3u8" type="application/x-mpegURL"></video>',
-    // <video width="320" height="240" controls>
-    //   <source src="https://stream.mux.com/pZUcG8r5kbDUNSpFoKgvfgaFAe01ZUE9FYyxSb9piCy4.m3u8" type="application/x-mpegURL">
-    // </video>
-    // get: () => '<script>$.getScript("https://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js", () => { this.set("loaded", true); }</script><video class="video-js" controls data-setup="{}"><source src="https://stream.mux.com/pZUcG8r5kbDUNSpFoKgvfgaFAe01ZUE9FYyxSb9piCy4.m3u8" type="application/x-mpegURL"></video><script>$.getScript("https://vjs.zencdn.net/7.10.2/video.js", () => { this.set("loaded", true); }</script>',
-  }],
+  fields: [],
   segments: [],
 });
